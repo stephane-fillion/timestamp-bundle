3e9f27a9fbb560794f54aae300da7ee34210b327
Stéphane Fillion <s.fillion@ameos.com>
2026-02-07 15:32:08 +0100

    Chargement de la configuration

--- src/TimestampBundle.php
+++ src/TimestampBundle.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace TimestampBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class TimestampBundle extends Bundle
+class TimestampBundle extends AbstractBundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.yaml');
     }
 }
