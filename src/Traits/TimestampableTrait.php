e832a3de59573d18b2f460f4d1b20048290d1889
Sam LABBE <github@slabb.dev>
2026-02-07 16:56:39 +0100

    fix: corrections code review

--- src/Traits/TimestampableTrait.php
+++ src/Traits/TimestampableTrait.php
@@ -6,24 +6,12 @@ namespace TimestampBundle\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
-use TimestampBundle\Attribute\Timestampable;
 
 trait TimestampableTrait
 {
-    /**
-     * FIX: Utiliser DATETIME_IMMUTABLE et non DATE_IMMUTABLE
-     * 
-     * DATE_IMMUTABLE = stocke uniquement la date (ex: 2026-02-07)
-     * DATETIME_IMMUTABLE = stocke date + heure (ex: 2026-02-07 15:30:45)
-     * 
-     * Pour un timestamp de création/modification, on veut l'heure précise.
-     */
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $createdAt = null;
 
-    /**
-     * FIX: Même correction - DATETIME_IMMUTABLE au lieu de DATE_IMMUTABLE
-     */
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
